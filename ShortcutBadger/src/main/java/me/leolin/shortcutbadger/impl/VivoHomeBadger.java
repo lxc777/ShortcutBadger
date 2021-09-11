7b8c8e588df16e96659c452f0c417fb86a8438c3
刘兴驰 <329392707@qq.com>
2021-09-11 22:47:10 +0800

    修改viov
    添加权限

--- ShortcutBadger/src/main/java/me/leolin/shortcutbadger/impl/VivoHomeBadger.java
+++ ShortcutBadger/src/main/java/me/leolin/shortcutbadger/impl/VivoHomeBadger.java
@@ -1,8 +1,10 @@
 package me.leolin.shortcutbadger.impl;
 
+import android.annotation.SuppressLint;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,12 +17,16 @@ import me.leolin.shortcutbadger.ShortcutBadgeException;
  */
 public class VivoHomeBadger implements Badger {
 
+    @SuppressLint("WrongConstant")
     @Override
     public void executeBadge(Context context, ComponentName componentName, int badgeCount) throws ShortcutBadgeException {
         Intent intent = new Intent("launcher.action.CHANGE_APPLICATION_NOTIFICATION_NUM");
         intent.putExtra("packageName", context.getPackageName());
         intent.putExtra("className", componentName.getClassName());
         intent.putExtra("notificationNum", badgeCount);
+        if (Integer.parseInt(Build.VERSION.RELEASE) > 8) {
+            intent.addFlags(0x01000000);
+        }
         context.sendBroadcast(intent);
     }
 
